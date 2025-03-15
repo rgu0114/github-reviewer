e28dc35870751578efe77265cb1c36792a501066
Anthony Sistilli <sistillianthony@gmail.com>
2025-03-15 18:19:05 -0400

    delete

--- src/index.js
+++
@@ -1,9 +0,0 @@
-import * as dotenv from "dotenv";
-import { startServer } from "./webhook-server";
-// Load environment variables
-dotenv.config();
-// Start the webhook server
-startServer().catch((error) => {
-    console.error("Failed to start server:", error);
-    process.exit(1);
-});
